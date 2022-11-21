117d2504cf5042dd6d715dbc41e82bbd8aab208d
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 09:01:07 +0100

    Added creation of 'Publications' directory

--- KIT_script.R
+++ KIT_script.R
@@ -4,6 +4,7 @@ rootpath <- here::here()
 institutions <- c('umea','link','uppsala','orebro','gbg','lund','ki')
 
 dir.create(file.path(rootpath, 'Output'), showWarnings = FALSE)
+dir.create(file.path(rootpath, 'Publications'), showWarnings = FALSE)
 for (ins in institutions){
   print(paste0("Collecting data from ",ins,"..."))
   dir.create(file.path(rootpath, paste0('./Publications/',ins)), showWarnings = FALSE)
