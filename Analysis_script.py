01a76a2f83c4230c5060ddbd25157fcddc0ea761
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 09:49:22 +0100

    Placing all output in 'Output' directory

--- Analysis_script.py
+++ Analysis_script.py
@@ -29,22 +29,22 @@ all_primed.to_csv('Output/All.csv')
 columns=['PMID','is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution','Journal/Book']
 all_no_inst=pd.DataFrame(all_primed, columns=columns)
 df=all_no_inst.replace({'TRUE':True,'FALSE':False})
-df.to_csv('allswedishresearch.csv')
+df.to_csv('Output/All_Swedish_research.csv')
 
 years=[2017,2018,2019,2020,2021,2022]
 transpall=df[df['Publication Year'].isin(years)]
 grouped_transpall=round(transpall.groupby(['Publication Year','Institution']).mean()*100,1)
-grouped_transpall.to_csv('./Output/Transparency_grouped_by_institution.csv')
+grouped_transpall.to_csv('Output/Transparency_grouped_by_institution.csv')
 
 years=[2017,2018,2019,2020,2021,2022]
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year']
 df_k=df.drop_duplicates(subset=['PMID'])
 df_temp=pd.DataFrame(df_k, columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
-round(df_temp.groupby(['Publication Year']).mean()*100,1).to_csv('./General_transparency.csv')
+round(df_temp.groupby(['Publication Year']).mean()*100,1).to_csv('Output/General_transparency.csv')
 
 years=[2017,2018,2019,2020,2021,2022]
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution']
 df_temp=pd.DataFrame(df, columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
-round(df_temp.groupby(['Publication Year'])['Institution'].value_counts()).to_csv('./Output/Pubs_year.csv')
+round(df_temp.groupby(['Publication Year'])['Institution'].value_counts()).to_csv('Output/Pubs_year.csv')
