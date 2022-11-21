ebf4ecf398f3284556a91dbfeab146463a904472
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 08:46:24 +0100

    Separated KI-transparent into one file of functions and one script

--- KI_transparent.R
+++ KI_transparent.R
@@ -66,14 +66,3 @@ evaluate_transparency=function(ins){
   rest_df <- foreach::foreach(x = filelist,.combine='rbind.fill') %dopar%{rtransparent::rt_all_pmc(x)}
   write.csv(rest_df,paste0("./output/resttransp_",ins,".csv"), row.names = FALSE)
 }
-
-rootpath <- here::here()
-institutions <- c('umea','link','uppsala','orebro','gbg','lund','ki')
-
-dir.create(file.path(rootpath, 'output'), showWarnings = FALSE)
-for (ins in institutions){
-  print(paste0("Collecting data from ",ins,"..."))
-  dir.create(file.path(rootpath, paste0('./publications_',ins)), showWarnings = FALSE)
-  download_publication_data(ins)
-  evaluate_transparency(ins)
-}
