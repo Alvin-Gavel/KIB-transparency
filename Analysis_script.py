0e348c72b5ebb7afd6ae9d5255d093ec60caa7d4
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 11:26:34 +0100

    Removed redundant output
    It was exporting to files, but as far as I can tell they're basically the same.

--- Analysis_script.py
+++ Analysis_script.py
@@ -1,11 +1,16 @@
-import pandas as pd
+"""
+This script is intended to read the data output by the R script Collection_script.
+"""
+
 import os
+
+import pandas as pd
 import numpy as np
 
 #institutions=['lund','ki','uppsala','umea','orebro','link','gbg']
 institutions=['gbg']
 
-# Collect data
+# Combine data from the three lists we use
 research = []
 for ins in institutions:
   code=pd.read_csv('./Output/Codesharing/{}.csv'.format(ins))
@@ -24,27 +29,21 @@ all_ins=pd.concat(research)
 all_ins.head()
 columns=['PMID','is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution','Journal/Book']
 all_primed=pd.DataFrame(all_ins, columns=columns)
+all_primed=all_primed.replace({'TRUE':True,'FALSE':False})
 all_primed.to_csv('Output/All.csv')
 
-columns=['PMID','is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution','Journal/Book']
-all_no_inst=pd.DataFrame(all_primed, columns=columns)
-df=all_no_inst.replace({'TRUE':True,'FALSE':False})
-df.to_csv('Output/All_Swedish_research.csv')
-
 years=[2017,2018,2019,2020,2021,2022]
-transpall=df[df['Publication Year'].isin(years)]
+transpall=all_primed[all_primed['Publication Year'].isin(years)]
 grouped_transpall=round(transpall.groupby(['Publication Year','Institution']).mean()*100,1)
 grouped_transpall.to_csv('Output/Transparency_grouped_by_institution.csv')
 
-years=[2017,2018,2019,2020,2021,2022]
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year']
-df_k=df.drop_duplicates(subset=['PMID'])
+df_k=all_primed.drop_duplicates(subset=['PMID'])
 df_temp=pd.DataFrame(df_k, columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
 round(df_temp.groupby(['Publication Year']).mean()*100,1).to_csv('Output/General_transparency.csv')
 
-years=[2017,2018,2019,2020,2021,2022]
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution']
-df_temp=pd.DataFrame(df, columns=columns)
+df_temp=pd.DataFrame(all_primed, columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
 round(df_temp.groupby(['Publication Year'])['Institution'].value_counts()).to_csv('Output/Pubs_year.csv')
