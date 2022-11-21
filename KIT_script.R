8260bb5ddfd6fa3170b301731324d0e9cf11f3e5
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 08:55:47 +0100

    Fixed bug in directory naming

--- KIT_script.R
+++ KIT_script.R
@@ -6,7 +6,7 @@ institutions <- c('umea','link','uppsala','orebro','gbg','lund','ki')
 dir.create(file.path(rootpath, 'output'), showWarnings = FALSE)
 for (ins in institutions){
   print(paste0("Collecting data from ",ins,"..."))
-  dir.create(file.path(rootpath, paste0('./publications_',ins)), showWarnings = FALSE)
+  dir.create(file.path(rootpath, paste0('./publications/',ins)), showWarnings = FALSE)
   download_publication_data(ins)
   evaluate_transparency(ins)
 }
