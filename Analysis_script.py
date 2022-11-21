b3b7c5558cf5e60a59f309be2a8875dd79f351e4
Alvin-Gavel <alvin.gavel@gmail.com>
2022-11-21 11:40:45 +0100

    Made code slightly more legible

--- Analysis_script.py
+++ Analysis_script.py
@@ -34,16 +34,17 @@ all_primed.to_csv('Output/All.csv')
 
 years=[2017,2018,2019,2020,2021,2022]
 transpall=all_primed[all_primed['Publication Year'].isin(years)]
-grouped_transpall=round(transpall.groupby(['Publication Year','Institution']).mean()*100,1)
-grouped_transpall.to_csv('Output/Transparency_grouped_by_institution.csv')
+grouped_by_ins=round(transpall.groupby(['Publication Year','Institution']).mean()*100,1)
+grouped_by_ins.to_csv('Output/Transparency_grouped_by_institution.csv')
 
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year']
-df_k=all_primed.drop_duplicates(subset=['PMID'])
-df_temp=pd.DataFrame(df_k, columns=columns)
+# I'm not sure about this one. Shouldn't we always drop duplicates?
+df_temp=pd.DataFrame(all_primed.drop_duplicates(subset=['PMID']), columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
 round(df_temp.groupby(['Publication Year']).mean()*100,1).to_csv('Output/General_transparency.csv')
 
 columns=['is_coi_pred','is_register_pred','is_fund_pred','is_open_code','is_open_data','Publication Year','Institution']
 df_temp=pd.DataFrame(all_primed, columns=columns)
 df_temp=df_temp[df_temp['Publication Year'].isin(years)]
-round(df_temp.groupby(['Publication Year'])['Institution'].value_counts()).to_csv('Output/Pubs_year.csv')
+grouped_by_year = round(df_temp.groupby(['Publication Year'])['Institution'].value_counts())
+grouped_by_year.to_csv('Output/Pubs_year.csv')
