6a1e59cd084d6585ebb99d1b37f7ba2f3dc14fd5
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-19 14:43:59 +0100

    Separated class into two
    There is now a more generic base class that describes any kind of connection to a SQL table, and a class that describes one specifically to a table of open access articles.

--- sql_table.py
+++ sql_table.py
@@ -3,19 +3,14 @@ import pandas as pd
 import psycopg2 as ps
 import wget
 
-class open_access_table:
+class sql_table:
     """
-    This class describes a table of open access files on Pubmed
-    Central.
+    This class describes a connection to a table in our SQL database
     """
     def __init__(self, table_name, config_path):
         self.table_name = table_name
         self.config_path = config_path
         self.config = self.configure()
-        
-        self.filename = 'oa_file_list.txt'
-        self.link = 'ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/' + self.filename
-        self.oa_table = None
         return
 
     def configure(self):
@@ -30,6 +25,47 @@ class open_access_table:
         for param in list(parser.items('database')):
             config[param[0]] = param[1]
         return config
+
+    def execute_sql_query(self, query):
+        """
+        Execute some arbitrary SQL query to the database specified
+        in the .config file.
+        """
+        # Code courtesy of https://www.postgresqltutorial.com/postgresql-python/create-tables/
+        conn = ps.connect(host = self.config['host'],
+                                database = self.config['database'],
+                                user = self.config['user'],
+                                password = self.config['password'],
+                                port = self.config['port'])
+        try:
+            params = self.config
+            conn = ps.connect(**params)
+            cur = conn.cursor()
+            cur.execute(query)
+            response = cur.fetchall()
+            cur.close()
+            conn.commit()
+        except (Exception, ps.DatabaseError) as error:
+            print(error)
+            response = None
+
+        if conn != None:
+            conn.close()
+        return response
+
+
+class open_access_table(sql_table):
+    """
+    This class describes a table of open access files on Pubmed
+    Central.
+    """
+    def __init__(self, table_name, config_path):
+        sql_table.__init__(self, table_name, config_path)
+
+        self.filename = 'oa_file_list.txt'
+        self.link = 'ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/' + self.filename
+        self.oa_table = None
+        return
 
     def get_file_list(self):
         """
@@ -49,31 +85,6 @@ class open_access_table:
         self.oa_table = self.oa_table[self.oa_table['pmid'].astype(bool)]
         return
 
-    def execute_sql_query(self, query):
-        """
-        Execute some arbitrary SQL query to the database specified
-        in the .config file.
-        """
-        # Code courtesy of https://www.postgresqltutorial.com/postgresql-python/create-tables/
-        conn = ps.connect(host = self.config['host'],
-                                database = self.config['database'],
-                                user = self.config['user'],
-                                password = self.config['password'],
-                                port = self.config['port'])        
-        try:
-            params = self.config
-            conn = ps.connect(**params)
-            cur = conn.cursor()
-            cur.execute(query)
-            cur.close()
-            conn.commit()
-        except (Exception, ps.DatabaseError) as error:
-            print(error)
-
-        if conn != None:
-            conn.close()
-        return
-
     def insert_in_db_table(self):
         """
         Creates a table of pmids and pmcids
@@ -81,7 +92,7 @@ class open_access_table:
         query = 'INSERT INTO {} (\n'.format(self.table_name)
         query +='pmid,\n'
         query +='pmcid\n'
-        query +=')\n' 
+        query +=')\n'
         query +='VALUES '
         rows = []
         for pmid, pmcid in zip(self.oa_table['pmid'], self.oa_table['pmcid']):
